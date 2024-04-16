7966004505ad56030d72e3112910757f1e67faf4
Corey Reed <reed.corey@gmail.com>
2024-04-15 23:20:22 -0400

    Add SVR analysis

--- helper_functions.py
+++ helper_functions.py
@@ -4,7 +4,43 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import KFold, cross_val_predict, cross_val_score
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+def full_training_set_scores(X_df, y_df, model):
+    """
+    Predicts on the same data set a model is fit to
+    Returns R-squared and RMSE scores
+
+    :param X_df: X dataframe, usually X_train
+    :param y_df: y dataframe, usually y_train
+    :param model: model used for fitting and predicting
+
+    :return: R-squared and RMSE scores
+    """
+    model = model
+    preds = model.predict(X_df)
+
+    r2 = r2_score(y_df, preds)
+    rmse = mean_squared_error(y_df, preds, squared=False)
+
+    return r2, rmse
+
+
+def print_scores(r2, rmse, model_name=None):
+    """
+    Prints scores from a model
+
+    :param r2: float, R-squared score
+    :param rmse: float, RMSE score
+    :param model_name: string, name of model used for scoring
+
+    :return: None
+    """
+    if model_name:
+        print(f'{model_name}:\n')
+    print(f'R-squared:     {r2}')
+    print(f'RMSE:          {rmse}')
 
 
 def kfold_validate_pred(X_df, y_df, model, num_folds=8):
